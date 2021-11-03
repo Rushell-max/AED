e7417031551856f10c026137079e3152b88f78b8
Ruzo <63430135+Rushell-max@users.noreply.github.com>
2021-11-03 09:13:22 -0500

    Update AVL_FE.cpp

--- AVL/AVL_FE.cpp
+++ AVL/AVL_FE.cpp
@@ -1,8 +1,8 @@
 //////////////////////////////////////////////////
 // NOMBRES Y APELLIDOS Rushell Vanessa Zavalaga Orozco
-// FECHA 13/10
 //////////////////////////////////////////////////
 
+// AVL CON Factor de equilibrio (FE) 
 #include<iostream>
 #include <cmath>
 #include <fstream>
@@ -65,6 +65,21 @@ class AVL_Tree
            
            p = q;
        }
+       // RII = 0 Y RDD = 1
+       void RS(Nodo<T> * &p, bool l)
+       {
+           Nodo<T> * q = p->m_pSon[l];
+           p->m_pSon[l] = q->m_pSon[!l];
+           q->m_pSon[!l] = p;
+
+           q->m_pSon[2] = p->m_pSon[2];
+           p->m_pSon[2] = q;
+           
+           if(q->FE == 1 || q->FE == -1) p->FE = q->FE = 0;
+           else if(q->FE == 0) p->FE = 1;
+
+           p=q; 
+       }
        void RDI(Nodo<T> * & p)
        {
            Nodo<T> *q = p->m_pSon[1];
@@ -72,13 +87,9 @@ class AVL_Tree
 
            p->m_pSon[1] = r->m_pSon[0];
            if(p->m_pSon[1]) (p->m_pSon[1])->m_pSon[2] = p; 
-           
-           balancearFE(p);
 
            q->m_pSon[0] = r->m_pSon[1];
            if(q->m_pSon[0]) (q->m_pSon[0])->m_pSon[2] = q; 
-
-           balancearFE(q);
            
            r->m_pSon[1] = q;
            r->m_pSon[0] = p;
@@ -87,6 +98,24 @@ class AVL_Tree
            q->m_pSon[2] = p->m_pSon[2] = r;
            
            r->FE = 0;
+
+           if (q->m_pSon[0] && p->m_pSon[1]){
+               if (q->m_pSon[1]) q->FE = 0;
+               else q->FE = -1;
+               if (p->m_pSon[0]) p->FE = 0;
+               else p->FE = 1;
+               r->FE = 0;
+           } else if (q->m_pSon[0]){
+               if (q->m_pSon[1]) q->FE = 0;
+               else q->FE = -1;
+               if (p->m_pSon[0]) { p->FE = -1; r->FE = 0; }
+               else p->FE = 0;
+           } else if (p->m_pSon[1]){
+               if (q->m_pSon[1]) { q->FE = 1; r->FE = 0; }
+               else q->FE = 0;
+               if (p->m_pSon[0]) p->FE = 0;
+               else p->FE = 1;
+           } else p->FE = q->FE = 0;
 
            p = r;    
        }
@@ -97,13 +126,9 @@ class AVL_Tree
 
            p->m_pSon[0] = r->m_pSon[1]; 
            if(p->m_pSon[0]) (p->m_pSon[0])->m_pSon[2] = p;
-    
-           balancearFE(p);
 
            q->m_pSon[1] = r->m_pSon[0];
            if(q->m_pSon[1]) (q->m_pSon[1])->m_pSon[2] = q; 
-
-           balancearFE(q);
 
            r->m_pSon[0] = q;
            r->m_pSon[1] = p;
@@ -112,46 +137,86 @@ class AVL_Tree
            q->m_pSon[2] = p->m_pSon[2] = r;
 
            r->FE = 0;
-
            
+           if (q->m_pSon[1] && p->m_pSon[0]) {
+               if(q->m_pSon[0]) q->FE = 0; 
+               else q->FE = 1;
+               if(p->m_pSon[1]) p->FE = 0;
+               else p->FE = -1;
+               r->FE = 0;
+           } else if(q->m_pSon[1]){
+               if(q->m_pSon[0]) q->FE = 0; 
+               else q->FE = 1;
+               if(p->m_pSon[1]) { p->FE = 1; r->FE = 0; }
+               else p->FE = 0;
+           } else if (p->m_pSon[0]){
+               if(q->m_pSon[0]){ q->FE = -1; r->FE = 0; }
+               else q->FE = 0;
+               if(p->m_pSon[1]) p->FE = 0;
+               else p->FE = -1;
+           } else p->FE = q->FE = 0;
+
            p = r;          
-        }
-
-        void balancearFE(Nodo<T> *p)
+       }
+        // RID = 0 Y RDI = 1
+        void RD(Nodo<T> * &p, bool l)
         {
-           if(p->m_pSon[0] && p->m_pSon[1]) 
-           {
-               int a=0, b=0;
-               if(p->m_pSon[0]->FE<0) a = p->m_pSon[0]->FE*-1;
-               a = p->m_pSon[0]->FE;
-               if(p->m_pSon[1]->FE<0) b = p->m_pSon[1]->FE*-1;
-               b = p->m_pSon[1]->FE; 
+            Nodo<T> * q =  p->m_pSon[l];
+            Nodo<T> * r =  q->m_pSon[!l];
+            
+            p->m_pSon[l] = r->m_pSon[!l];
+            if(p->m_pSon[l]) (p->m_pSon[l])->m_pSon[2] = p;
 
-               p->FE = b - a;
-           }
-           else if(!p->m_pSon[0] && p->m_pSon[1])
-           {
-               int b;
-               if(p->m_pSon[1]->FE<0) b = p->m_pSon[1]->FE*-1;
-               b = p->m_pSon[1]->FE;
-               p->FE = 1 + b; // (b - a) + 1
-           }  
-           else if(p->m_pSon[0] && !p->m_pSon[1])
-           {
-               int a;
-               if(p->m_pSon[0]->FE<0) a = p->m_pSon[0]->FE*-1;
-               a = p->m_pSon[0]->FE;
-               p->FE = (0 - a) - 1;
-           }  
-           else 
-           {
-               p->FE = 0;
-           }  
-
+            q->m_pSon[!l] = r->m_pSon[l];
+            if(p->m_pSon[!l]) (p->m_pSon[!l])->m_pSon[2] = p;
+            
+            r->m_pSon[l] = q;
+            r->m_pSon[!l] = p;
+            
+            r->m_pSon[2]= p->m_pSon[2];
+            q->m_pSon[2] = p->m_pSon[2] = r;
+            
+            if(p->FE == 2) {
+                if (q->m_pSon[0] && p->m_pSon[1]){
+                    if (q->m_pSon[1]) q->FE = 0;
+                    else q->FE = -1;
+                    if (p->m_pSon[0]) p->FE = 0;
+                    else p->FE = 1;
+                    r->FE = 0;
+                } else if (q->m_pSon[0]){
+                    if (q->m_pSon[1]) q->FE = 0;
+                    else q->FE = -1;
+                    if (p->m_pSon[0]) { p->FE = -1; r->FE = 0; }
+                    else p->FE = 0;
+                } else if (p->m_pSon[1]){
+                    if (q->m_pSon[1]) { q->FE = 1; r->FE = 0; }
+                    else q->FE = 0;
+                    if (p->m_pSon[0]) p->FE = 0;
+                    else p->FE = 1;
+                } else p->FE = q->FE = 0;
+            }else {
+                if (q->m_pSon[1] && p->m_pSon[0]) {
+                    if(q->m_pSon[0]) q->FE = 0; 
+                    else q->FE = 1;
+                    if(p->m_pSon[1]) p->FE = 0;
+                    else p->FE = -1;
+                    r->FE = 0;
+                } else if(q->m_pSon[1]){
+                    if(q->m_pSon[0]) q->FE = 0; 
+                    else q->FE = 1;
+                    if(p->m_pSon[1]) { p->FE = 1; r->FE = 0; }
+                    else p->FE = 0;
+                } else if (p->m_pSon[0]){
+                    if(q->m_pSon[0]){ q->FE = -1; r->FE = 0; }
+                    else q->FE = 0;
+                    if(p->m_pSon[1]) p->FE = 0;
+                    else p->FE = -1;                    
+                } else p->FE = q->FE = 0;
+            }
+            
+            p = r;        
         }
-       
 
-       
 
        public:
        AVL_Tree() {m_pRoot = 0;}
@@ -171,34 +236,46 @@ class AVL_Tree
                 if(p->FE == 0) return false;
                 if(p->FE > 1)
                 {
-                    /**print();**/
+                    //Print();
                     Nodo<T>* p_tmp = p->m_pSon[1];
-                    if(p_tmp->FE >= 1) { cout<<"\n EN RDD con "<<p->m_Dato<<"\n"; RDD(p);/** print();**/ return false; }
-                    else if(p_tmp->FE <= 1) { cout<<"\n EN RDI con "<<p->m_Dato<<"\n"; RDI(p); /**print();**/ return false;}  
+                    if(p_tmp->FE >= 1) { /**cout<<"\n EN RDD con "<<p->m_Dato<<"\n";**/ RS(p,1);/**RDD(p);/** Print();**/ return false; }
+                    else if(p_tmp->FE <= 1) { /**cout<<"\n EN RDI con "<<p->m_Dato<<"\n";**/ RD(p,1);/**RDI(p);/**Print(); **/ return false;}  
                 }
                 else if( p->FE < -1)
                 {
-                    //print();
+                    //Print();
                     Nodo<T>* p_tmp = p->m_pSon[0];
-                    if(p_tmp->FE >= 1) {cout<<"\n EN RID con "<<p->m_Dato<<"\n"; RID(p); /**print();**/ return false;}
-                    else if(p_tmp->FE <= 1){cout<<"\n EN RII con "<<p->m_Dato<<"\n"; RII(p); /**print();**/ return false; }  
+                    if(p_tmp->FE >= 1) {/**cout<<"\n EN RID con "<<p->m_Dato<<"\n";**/ RD(p,0); /**RID(p); /**Print();**/ return false;}
+                    else if(p_tmp->FE <= 1){/**cout<<"\n EN RII con "<<p->m_Dato<<"\n";**/ RS(p,0);/**RII(p);/**Print();**/ return false; }  
                 }
                 return true;
             }
             return false;
             
        }
-       void print()
+       void Print()
        {
-           print(m_pRoot);
+           Print(m_pRoot);
        }
-       void print(Nodo<T> *r)
+       void Print(Nodo<T> *r)
        {
-            if(!r) return;
-            cout<<" FE> "<<r->FE<<" dir> "<<r<<" _ data> "<<r->m_Dato<<" _ dirdirLEFT> "<<&r->m_pSon[0]<<" _ dirdirRIGHT> "<<&r->m_pSon[1]<<" _ dirdirPADRE> "<<&r->m_pSon[2]<<" _ dirLEFT> "<<r->m_pSon[0]<<" _dirRIGHT> "<<r->m_pSon[1]<<" _dirPADRE> "<<r->m_pSon[2]<<"\n";
-            print(r->m_pSon[0]);
-            print(r->m_pSon[1]);           
+           if (!r) return;
+           cout << " FE> " << r->FE << " dir> " << r << " _ data> " << r->m_Dato << " _ dirdirLEFT> " << &r->m_pSon[0] << " _ dirdirRIGHT> " << &r->m_pSon[1] << " _ dirdirPADRE> " << &r->m_pSon[2] << " _ dirLEFT> " << r->m_pSon[0] << " _dirRIGHT> " << r->m_pSon[1] << " _dirPADRE> " << r->m_pSon[2] << "\n";
+           Print(r->m_pSon[0]);
+           Print(r->m_pSon[1]);
        }
+
+       void Print(Nodo<T> *r, ostream &os)
+       {
+           if (!r) return;
+           os << r->m_Dato << "[label = \"" << r->m_Dato << " | " << r->FE << "\" ]; \n";
+           if (r->m_pSon[0]) os << r->m_Dato << "--" << r->m_pSon[0]->m_Dato << ";" << endl;
+           if (r->m_pSon[1]) os << r->m_Dato << "--" << r->m_pSon[1]->m_Dato << ";" << endl;
+           Print(r->m_pSon[0], os);
+           Print(r->m_pSon[1], os);
+       }
+       void Print(ostream &os) { Print(m_pRoot, os); }
+
        void dibujar()
        {
            ofstream archivo;
@@ -208,29 +285,27 @@ class AVL_Tree
            archivo << "\n}";
            archivo.close();
            system("E: & cd Graphviz & cd bin & dot AVL_HEIGHT.dot -o AVL_HEIGHT.png -Tpng & AVL_HEIGHT.png");
-        }
-       void dibujar(Nodo<T> *r, ofstream & archivo)
-        {
-            if(!r) return;
-            archivo << r->m_Dato << ";\n ";
-            if(r->m_pSon[0]) archivo << r->m_Dato << " -- " << r->m_pSon[0]->m_Dato << ";\n";
-            if(r->m_pSon[1]) archivo << r->m_Dato << " -- " << r->m_pSon[1]->m_Dato << ";\n";
-            dibujar(r->m_pSon[0], archivo);
-            dibujar(r->m_pSon[1], archivo);
-        }
-       bool Find (T D)
-       {
-            if(!m_pRoot) return false;
-            Nodo<T> *p_tmp = m_pRoot;
-            while(p_tmp)
-            {
-                if(p_tmp->m_Dato == D) return true;
-                p_tmp = p_tmp->m_pSon[p_tmp->m_Dato < D];
-            }
-            return NULL;
        }
-
-
+       void dibujar(Nodo<T> *r, ofstream &archivo)
+       {
+           if (!r) return;
+           archivo << r->m_Dato << ";\n ";
+           if (r->m_pSon[0]) archivo << r->m_Dato << "_" << r->FE << " -- " << r->m_pSon[0]->m_Dato << "_" << r->FE << ";\n";
+           if (r->m_pSon[1]) archivo << r->m_Dato << "_" << r->FE << " -- " << r->m_pSon[1]->m_Dato << "_" << r->FE << ";\n";
+           dibujar(r->m_pSon[0], archivo);
+           dibujar(r->m_pSon[1], archivo);
+       }
+       bool Find(T D)
+       {
+           if (!m_pRoot) return false;
+           Nodo<T> *p_tmp = m_pRoot;
+           while (p_tmp)
+           {
+               if (p_tmp->m_Dato == D) return true;
+               p_tmp = p_tmp->m_pSon[p_tmp->m_Dato < D];
+           }
+           return NULL;
+       }
 };
 
 
@@ -238,9 +313,9 @@ class AVL_Tree
 int test1()
 {
     AVL_Tree<int> A; 
-    A.Add(4);   A.print();
-    A.Add(14);  A.print();
-    A.Add(24);  A.print();
+    A.Add(4);   A.Print();
+    A.Add(14);  A.Print();
+    A.Add(24);  A.Print();
     cout<<"The test1 work perfectly. Nice work :)"<<endl;
    
     return 1;
@@ -249,9 +324,9 @@ int test1()
 int test2()
 {
     AVL_Tree<int> A; 
-    A.Add(24);   A.print();
-    A.Add(14);   A.print();
-    A.Add(2);    A.print();
+    A.Add(24);   A.Print();
+    A.Add(14);   A.Print();
+    A.Add(2);    A.Print();
     cout<<"The test2 work perfectly. Good Work :)"<<endl;
     return 1;
 }
@@ -259,9 +334,9 @@ int test2()
 int test3()
 {
     AVL_Tree<int> A; 
-    A.Add(4);   A.print();
-    A.Add(14);  A.print();
-    A.Add(10);  A.print();
+    A.Add(4);   A.Print();
+    A.Add(14);  A.Print();
+    A.Add(10);  A.Print();
     cout<<"The test3 work perfectly. Good Work :)"<<endl;
     return 1;
 }
@@ -269,9 +344,9 @@ int test3()
 int test4()
 {
     AVL_Tree<int> A; 
-    A.Add(24);  A.print();
-    A.Add(14);  A.print();
-    A.Add(18);  A.print();
+    A.Add(24);  A.Print();
+    A.Add(14);  A.Print();
+    A.Add(18);  A.Print();
     cout<<"The test4 work perfectly. Good Work :)"<<endl;
     return 1;
 }
@@ -279,10 +354,11 @@ int test4()
 // FINAL
 int test6()
 {
+    ofstream s("salida.dot");
     AVL_Tree<int> A; 
     // Insert the correct includes
     srand(10); 
-    for(int i=0;i<100;i++)
+    for(int i=0;i<1000;i++)
     {
         // Insert the correct includes 
         int a=rand()%1000;
@@ -291,13 +367,15 @@ int test6()
     }
 
 
-    A.print();
-    A.dibujar();
+    A.Print(s);
+   // A.dibujar();
 
     return 1;
 }
 
  
+
+
 int main()
 {
     cout<<"Begin test "<<endl;
